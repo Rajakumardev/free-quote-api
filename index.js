79a68ece9ffabd6f44bc894f4d7abf2fcc328f7f
Rajakumardev <rajakumarofficial@gmail.com>
2020-09-13 13:38:25 +0530

    functional Quotes api initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const PORT = process.env.PORT || 3000;
+let quoteObj = {};
+fs.readFile('./db/quotes.json','utf8',(err,data)=>{
+    if(err){
+        console.log(`quotes db read failed.`);
+        throw err;
+    }
+    quoteObj = JSON.parse(data);
+    console.log(`quotes db read successfull.`);
+});
+
+app.get('/api',(req,res)=>{
+    res.json(quoteObj);
+})
+
+app.get('/api/random',(req,res)=>{
+    const randomIndex = Math.round(Math.random() * quoteObj.length);
+    res.json(quoteObj[randomIndex]);
+});
+
+app.get('/api/getTotalQuotesCount',(req,res)=>{
+    res.json({
+        "count":quoteObj.length
+    })
+});
+
+app.listen(PORT,(err)=>{
+    if(err) throw err;
+    console.log(`Server started on PORT:${PORT}`);
+})
